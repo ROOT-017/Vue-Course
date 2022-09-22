e0c2a1d3a18b4cf8cc511b4a976241603e044fec
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2022-09-22 02:40:55 +0100

    Profile image componentcreated

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="Image" alt="" class="w-8 h-8 rounded-xl object-contain" />
+</template>
+
+<script>
+import profileImage from "@/assets/TERENCETOON.png";
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      Image: profileImage,
+    };
+  },
+};
+</script>
