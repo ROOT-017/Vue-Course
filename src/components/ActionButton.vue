614b3b45ebe4fb6563eff348d1ff0074f0403a7a
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2022-09-24 02:40:10 +0100

    Signin button created

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <button :class="buttonClass">
+    {{ buttonText }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ActionButton",
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: "primary",
+      validator(value) {
+        return ["primary", "secondary"].includes(value);
+      },
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    buttonClass() {
+      return {
+        [this.type]: true,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  @apply px-5 py-3 font-medium;
+}
+.primary {
+  @apply text-white rounded bg-brand-blue-1 hover:shadow-blue-1;
+}
+.secondary {
+  @apply text-brand-blue-1 bg-transparent hover:bg-brand-blue-2 hover:text-white;
+}
+</style>
