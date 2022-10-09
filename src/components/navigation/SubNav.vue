599a2f39d5902236555323b4331ee39fe0cb2631
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2022-10-09 04:29:57 +0100

    SubNav component edted

--- src/components/navigation/SubNav.vue
+++ src/components/navigation/SubNav.vue
@@ -1,21 +1,40 @@
 <template>
   <div class="w-full h-16 bg-white border-b border-solid border-band-gray-1">
     <div class="flex items-center h-full px-8">
-      <div v-if="onJobPage" data-test="job-count">
+      <div v-if="onJobPageComputed" data-test="job-count">
         <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
+
+        <span
+          ><span class="text-brand-green-1"
+            >{{ FILTERED_JOBS_BY_ORGANISATIONS.length + " " }} </span
+          >Jobs match</span
+        >
       </div>
-      <span><span class="text-brand-green-1">1653 </span>Jobs match</span>
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import { FILTERED_JOBS_BY_ORGANISATIONS } from "@/store/contants";
 export default {
   name: "SubNav",
-  data() {
-    return {
-      onJobPage: true,
-    };
+  // data() {
+  //   return {
+  //     onJobPage: true,
+  //   };
+  // },
+
+  computed: {
+    onJobPageComputed() {
+      if (this.$route.path.includes("jobs/results")) {
+        return true;
+      }
+      return false;
+    },
+    ...mapGetters([FILTERED_JOBS_BY_ORGANISATIONS]),
   },
+
+  methods: {},
 };
 </script>
