27d8273a0d81ccb5e1480f3817b18e10fbdc7344
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2022-10-17 04:32:20 +0100

    Accordion component modified

---
+++ src/shared/AccordionVue.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="py-5 border-b border-solid border-brand-gray-2-">
+    <div class="flex flex-wrap justify-between cursor-pointer" @click="open">
+      <h3 class="text-base font-semibold">{{ header }}</h3>
+      <font-awesome-icon :icon="caretIcon" />
+    </div>
+    <div v-if="isOpen" class="w-full mt-5">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AccordionVue",
+  props: {
+    header: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  computed: {
+    caretIcon() {
+      return this.isOpen ? ["fas", "angle-up"] : ["fas", "angle-down"];
+    },
+  },
+  methods: {
+    open() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
+</script>
