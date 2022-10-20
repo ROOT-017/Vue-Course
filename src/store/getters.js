2db0b125a6fe89b2c094fae992e0cc1cc6e8beb2
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2022-10-20 04:26:35 +0100

    Getter file isolated from store/index.js file

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,9 @@
 import {
   UNIQUE_ORGANISATIONS,
   FILTERED_JOBS_BY_ORGANISATIONS,
+  UNIQUE_JOB_TYPES,
+  FILTERED_JOBS_BY_JOB_TYPES,
+  FILTERED_JOBS,
 } from "@/store/contants";
 
 const getters = {
@@ -17,6 +20,34 @@ const getters = {
     return state.jobs.filter((job) =>
       state.selectedOrganisations.includes(job.organization)
     );
+  },
+  [UNIQUE_JOB_TYPES](state) {
+    const uniqueJobTypes = new Set();
+    state.jobs.forEach((job) => uniqueJobTypes.add(job.jobType));
+    return uniqueJobTypes;
+  },
+  [FILTERED_JOBS_BY_JOB_TYPES](state) {
+    if (state.selectedJobTypes.length === 0) {
+      return state.jobs;
+    }
+    return state.jobs.filter((job) =>
+      state.selectedJobTypes.includes(job.jobType)
+    );
+  },
+  [FILTERED_JOBS](state) {
+    const noSelectedOrganisation = state.selectedOrganisations.length === 0;
+    const noSelectedJobtypes = state.selectedJobTypes.length === 0;
+
+    if (noSelectedOrganisation && noSelectedJobtypes) return state.jobs;
+    return state.jobs
+      .filter((job) => {
+        if (noSelectedOrganisation) return true;
+        return state.selectedOrganisations.includes(job.organization);
+      })
+      .filter((job) => {
+        if (noSelectedJobtypes) return true;
+        return state.selectedJobTypes.includes(job.jobType);
+      });
   },
 };
 
